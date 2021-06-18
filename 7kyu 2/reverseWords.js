98c72e37b45dd8b5a5ad53871b377add3c80ce35
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-18 16:16:36 +0700

    7.37

---
+++ 7kyu 2/reverseWords.js
@@ -0,0 +1,27 @@
+function reverseWords(str) {
+   let res = [];
+   const arr = str.split(" ");
+   for (let i = 0; i < arr.length; i++) {
+      res.push(arr[i].split("").reverse().join(""));
+   }
+   return res.join(" ");
+}
+
+
+const reverseWords = s => s.replace(/\S+/g, v => [...v].reverse().join ``);
+
+
+function reverseWords(str) {
+   return str.split(' ').map(str => str.split('').reverse().join('')).join(' ');
+}
+
+
+function reverseWords(str) {
+   return str.split("").reverse().join("").split(" ").reverse().join(" ");
+}
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'), '|| ehT kciuq nworb xof spmuj revo eht yzal .god');
+console.log(reverseWords('apple'), '|| elppa');
+console.log(reverseWords('a b c d'), '|| a b c d');
+console.log(reverseWords('double  spaced  words'), '|| elbuod  decaps  sdrow');
