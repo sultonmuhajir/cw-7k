53816a069bd56ce3c96d2ea97135e14b07a1a966
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-24 22:21:51 +0700

    7.42

---
+++ 7kyu 1/getEvenNumbers.js
@@ -0,0 +1,27 @@
+function getEvenNumbers(numbersArray) {
+   return numbersArray.filter(i => i % 2 == 0);
+}
+
+
+const getEvenNumbers = arr => arr.filter(x => !(x & 1));
+
+
+function getEvenNumbers(numbersArray) {
+   const result = [];
+   for (let number of numbersArray) {
+      if (number % 2 === 0) {
+         result.push(number);
+      }
+   }
+   return result;
+}
+
+
+const getEvenNumbers = _ => _.filter(__ => __ % 2 == 0);
+
+
+console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]), [2, 6, 8, 10])
+console.log(getEvenNumbers([1, 2]), [2])
+console.log(getEvenNumbers([12, 14, 15]), [12, 14])
+console.log(getEvenNumbers([13, 15]), [])
+console.log(getEvenNumbers([1, 3, 9]), [])
