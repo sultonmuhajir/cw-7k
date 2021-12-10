d00523a60b2bc24d82cfd5ee7d4b9b96ce2ae6f3
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-12-10 09:57:17 +0700

    83.49.71

---
+++ Python/7py 1/arrow_area.py
@@ -0,0 +1,26 @@
+"""
+|--------------------------------------------------------------------------
+| Area of an arrow
+|--------------------------------------------------------------------------
+|
+"""
+
+def arrow_area(a, b):
+   return (a/2) * (b/2)
+
+
+def arrow_area(a, b):
+   return a*b/4
+
+
+def arrow_area(a, b):
+   return a * b * 0.25
+
+
+def arrow_area(a, b):
+   return 0.5 * 0.5 * b * a
+
+
+print(arrow_area(4,2), 2)
+print(arrow_area(7,6), 10.5)
+print(arrow_area(25,25), 156.25)
