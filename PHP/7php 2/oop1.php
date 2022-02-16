fa1d8653a79eab7e92cefb1fac6a385ecc46b27c
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-02-16 09:52:42 +0700

    97.70.97

---
+++ PHP/7php 2/oop1.php
@@ -0,0 +1,22 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Object-Oriented PHP #1 - Classes, Public Properties and Methods
+|--------------------------------------------------------------------------
+|
+*/
+
+class President {
+   public $name = "Barack Obama";
+   
+   public function greet($name) {
+      return "Hello {$name}, my name is Barack Obama, nice to meet you!";
+   }
+}
+
+
+$us_president = new President;
+$president_name = $us_president->name;
+$greetings_from_president = $us_president->greet("Donald");
+
+echo $greetings_from_president;
