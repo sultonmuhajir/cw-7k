880a49d5a20eea42ef0b730b94862287c942ebb5
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-08-22 08:27:12 +0700

    162.140.162

---
+++ Python/7py 3/over_the_road.py
@@ -0,0 +1,26 @@
+"""
+|--------------------------------------------------------------------------
+| Over The Road
+|--------------------------------------------------------------------------
+|
+"""
+
+def over_the_road(address, n):
+   return n * 2 + 1 - address
+
+
+def over_the_road(address, n):
+   return n * 2 - address + 1
+
+
+def over_the_road(address, n):
+   return n + n - ~- address
+
+
+def over_the_road(address, n):
+   return -2 * ~ n - 1 - address
+
+
+print(over_the_road(2, 3), 5)
+print(over_the_road(3, 5), 8)
+print(over_the_road(7, 11), 16)
