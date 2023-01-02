d262c7bd0e0f4025cee66886601d6c32285a8553
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-01-02 13:42:52 +0700

    202.182.201

---
+++ JavaScript/7js 4/rowSumOddNumbers.js
@@ -0,0 +1,22 @@
+/*
+|--------------------------------------------------------------------------
+| Sum of odd numbers
+|--------------------------------------------------------------------------
+|
+*/
+
+function rowSumOddNumbers(n) {
+   return n ** 3;
+}
+
+
+const rowSumOddNumbers = (n) => n * n * n;
+
+
+function rowSumOddNumbers(n) {
+   return Math.pow(n, 3);
+}
+
+
+console.log(rowSumOddNumbers(1), 1);
+console.log(rowSumOddNumbers(42), 74088);
