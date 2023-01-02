9170ce36c8efbdcc6577481d8cc69106144ac12e
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-01-02 13:43:13 +0700

    202.182.202

---
+++ Python/7py 4/row_sum_odd_numbers.py
@@ -0,0 +1,24 @@
+"""
+|--------------------------------------------------------------------------
+| Sum of odd numbers
+|--------------------------------------------------------------------------
+|
+"""
+
+def row_sum_odd_numbers(n):
+   return n ** 3
+
+
+def row_sum_odd_numbers(n):
+   return n*n*n
+
+
+def row_sum_odd_numbers(n):
+   return pow(n, 3)
+
+
+print(row_sum_odd_numbers(1), 1)
+print(row_sum_odd_numbers(2), 8)
+print(row_sum_odd_numbers(13), 2197)
+print(row_sum_odd_numbers(19), 6859)
+print(row_sum_odd_numbers(41), 68921)
