b273a484991094339c01adbaf6181051a4130098
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-01-02 13:43:39 +0700

    202.183.202

---
+++ PHP/7php 3/rowSumOddNumbers.php
@@ -0,0 +1,27 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Sum of odd numbers
+|--------------------------------------------------------------------------
+|
+*/
+
+function rowSumOddNumbers($n) {
+   return $n ** 3;
+}
+
+
+function rowSumOddNumbers($n) {
+   return $n*$n*$n;
+}
+
+
+function rowSumOddNumbers($n) {
+   return pow($n, 3);
+}
+
+
+print_r(rowSumOddNumbers(1) . ' ' . 1 . '
+');
+print_r(rowSumOddNumbers(42) . ' ' . 74088 . '
+');
